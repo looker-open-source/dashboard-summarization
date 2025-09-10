e2d4dc2b232f925918e54cf2252e1374d00ebb7d
Brett Guenther <bguenther@google.com>
2025-09-10 11:34:26 +0800

    clean up typings

--- src/utils/fetchDashboardDetails.ts
+++ src/utils/fetchDashboardDetails.ts
@@ -1,11 +1,11 @@
 import { Filters } from "@looker/extension-sdk";
-import { DashboardMetadata, Query } from "../types";
+import { DashboardMetadata, Query, DashboardElement } from "../types";
 
-const applyFilterToListeners = (data, filters, dashboardFilters) => {
-  if (dashboardFilters !== null) {
-    const filterListeners = data.filter((item) => item.listen.length > 0);
-    filterListeners.forEach((filter) => {
-      filter.listen.forEach((listener) => {
+const applyFilterToListeners = (data: any, filters: any, dashboardFilters: any) => {
+  if (dashboardFilters !== null && data) {
+    const filterListeners = data.filter((item: any) => item.listen.length > 0);
+    filterListeners.forEach((filter: any) => {
+      filter.listen.forEach((listener: any) => {
         filters[listener.field] = dashboardFilters[listener.dashboard_filter_name];
       });
     });
@@ -23,7 +23,7 @@ export const fetchDashboardDetails = async (
     console.log("from function: ", dashboardId,dashboardFilters)
   const { description } = await core40SDK.ok(core40SDK.dashboard(dashboardId, 'description'));
 
-  let queries: any;
+  let queries: DashboardElement[];
   if (dashboardId.includes('::')) {
     const { dashboard_elements } = await core40SDK.ok(core40SDK.dashboard(dashboardId, 'dashboard_elements'));
     queries = dashboard_elements;
@@ -32,23 +32,19 @@ export const fetchDashboardDetails = async (
       dashboardId, 'query,result_maker,note_text,title,query_id'));
   }
 
-  queries = queries.filter((d: any) => d.query !== null || d.result_maker !== null)
-    .map((data: any) => {
-      const { query, note_text, title } = data;
-      if (data.query !== null) {
-        const { fields, dynamic_fields, view, model, filters, pivots, sorts, limit, column_limit, row_total, subtotals } = query;
-        const newFilters = applyFilterToListeners(data.result_maker?.filterables, filters || {}, dashboardFilters);
-        return { queryBody: { fields, dynamic_fields, view, model, filters: newFilters, pivots, sorts, limit, column_limit, row_total, subtotals }, note_text, title };
-      } else if (data.result_maker?.query !== null) {
-        const { fields, dynamic_fields, view, model, filters, pivots, sorts, limit, column_limit, row_total, subtotals } = data.result_maker!.query;
-        const newFilters = applyFilterToListeners(data.result_maker?.filterables, filters || {}, dashboardFilters);
-        return { queryBody: { fields, dynamic_fields, view, model, filters: newFilters, pivots, sorts, limit, column_limit, row_total, subtotals }, note_text, title };
-      } else {
-        return undefined;
-      }
-    });
+  const mappedQueries: Query[] = queries.reduce((acc: Query[], data: DashboardElement) => {
+    const { note_text, title } = data;
+    const querySource = data.query || data.result_maker?.query;
 
-  await extensionSDK.localStorageSetItem(`${dashboardId}:${JSON.stringify(dashboardFilters)}`, JSON.stringify({ dashboardFilters, dashboardId, queries, description }));
+    if (querySource) {
+      const { fields, dynamic_fields, view, model, filters, pivots, sorts, limit, column_limit, row_total, subtotals } = querySource;
+      const newFilters = applyFilterToListeners(data.result_maker?.filterables, filters || {}, dashboardFilters);
+      acc.push({ queryBody: { fields, dynamic_fields, view, model, filters: newFilters, pivots, sorts, limit, column_limit, row_total, subtotals }, note_text, title });
+    }
+    return acc;
+  }, []);
+
+  await extensionSDK.localStorageSetItem(`${dashboardId}:${JSON.stringify(dashboardFilters)}`, JSON.stringify({ dashboardFilters, dashboardId, queries: mappedQueries, description }));
   console.log({ dashboardFilters, dashboardId, queries, description })
-  return { dashboardFilters, dashboardId, queries, description };
+  return { dashboardFilters, dashboardId, queries: mappedQueries, description };
 };
